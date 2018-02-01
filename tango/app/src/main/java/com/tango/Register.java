d0b8d2c3caea1acfd51f68fce9280f41ae43a0a9
DanielPri <privorotskyd@gmail.com>
2018-01-31 22:21:50 -0500

    Added onclick for Register button that prompts when no text is entered.

--- tango/app/src/main/java/com/tango/Register.java
+++ tango/app/src/main/java/com/tango/Register.java
@@ -1,9 +1,14 @@
 package com.tango;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Register extends AppCompatActivity {
 
@@ -17,7 +22,23 @@ public class Register extends AppCompatActivity {
         final EditText rgUsername = (EditText) findViewById(R.id.rgUsername);
         final EditText rgPassword = (EditText) findViewById(R.id.rgPassword);
 
+        final TextView rgFailPrompt = (TextView) findViewById(R.id.rgFailPrompt);
+        rgFailPrompt.setText("");
+
         final Button rgRegister = (Button) findViewById(R.id.rgRegister);
+
+        rgRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Add data from each field into database
+                boolean promptFail = false;
+                if(rgName.getText().toString().equals("") || rgEmail.getText().toString().equals("") || rgUsername.getText().toString().equals("") || rgPassword.getText().toString().equals("")){promptFail = true;};
+                if(promptFail){
+                    rgFailPrompt.setText("YOU MUST FILL IN ALL TEXT FIELDS!");
+                    rgFailPrompt.setTextColor(Color.RED);
+                }
+            }
+        });
 
     }
 }
