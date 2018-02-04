04eb8ab3a9d2c50aeeea39ca7505ccfea037a09f
WolfOfTheNorth <davebhardwaj@gmail.com>
2018-02-04 15:50:18 -0500

    Update DatabaseHelper.java

--- tango/app/src/main/java/com/tango/DatabaseHelper.java
+++ tango/app/src/main/java/com/tango/DatabaseHelper.java
@@ -188,7 +188,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         /**
          * Here query function is used to fetch records from user table this function works like we use sql query.
          * SQL query equivalent to this query function is
-         * SELECT user_id FROM user WHERE user_email = 'jack@androidtutorialshub.com';
+         * SELECT user_id FROM user WHERE user_email = 'jack@mail.com';
          */
         Cursor cursor = db.query(TABLE_USER, //Table to query
                 columns,                    //columns to return
@@ -232,7 +232,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         /**
          * Here query function is used to fetch records from user table this function works like we use sql query.
          * SQL query equivalent to this query function is
-         * SELECT user_id FROM user WHERE user_email = 'jack@androidtutorialshub.com' AND user_password = 'qwerty';
+         * SELECT user_id FROM user WHERE user_email = 'jack@mail.com' AND user_password = 'qwerty';
          */
         Cursor cursor = db.query(TABLE_USER, //Table to query
                 columns,                    //columns to return
