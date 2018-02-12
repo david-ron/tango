9596bf4444099fc997d1eb534b804768d9c6de38
Charles <chanc09@gmail.com>
2018-02-11 22:12:00 -0500

    #40 able to accept and decline answers, through radio button and text

--- tango/app/src/main/java/com/tango/AnsweringQuestion.java
+++ tango/app/src/main/java/com/tango/AnsweringQuestion.java
@@ -1,13 +1,21 @@
 package com.tango;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.text.style.ForegroundColorSpan;
 
 import java.util.ArrayList;
 
@@ -21,6 +29,10 @@ public class AnsweringQuestion extends AppCompatActivity {
     String answers="";           // This is the text that will be displayed in the answer box
     ArrayList<String> listOfAnswers= new ArrayList<String>();  // The answers will be stored in a list for voting system it will be easy to reorganise the list
 
+    //******
+    private RadioButton acceptButton;
+    private RadioButton declineButton;
+    EditText numR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +49,11 @@ public class AnsweringQuestion extends AppCompatActivity {
         writtenAnswer = (EditText) findViewById(R.id.writtenAnswer);   // Answer in the text box
         displayedAnswer = (TextView) findViewById(R.id.answers);     // Answer in the main page
 
+
+        acceptButton = (RadioButton) findViewById(R.id.acceptButton);
+        declineButton = (RadioButton) findViewById(R.id.declineButton);
+        numR = (EditText) findViewById(R.id.numRadio);
+
         reply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -51,13 +68,59 @@ public class AnsweringQuestion extends AppCompatActivity {
 
                 listOfAnswers.add(writtenAnswer.getText().toString());      // Answers are added in the list
 
-                for(int i =0; i<listOfAnswers.size(); i++){          // Answers are stored one by one in a string
-                    answers+= "UserName"+(i+1)+"\n" +listOfAnswers.get(i)+"\n\n\n"; ///////////  saves username skips a line and displays answer
+
+                for (int i = 0; i < listOfAnswers.size(); i++) {          // Answers are stored one by one in a string
+                    answers += "UserName" + (i + 1) + "\n" + listOfAnswers.get(i) + "\n\n\n"; ///////////  saves username skips a line and displays answer
                 }
 
                 displayedAnswer.setText(answers);    // The string containing all the answers is displayed
+
                 textbox.setVisibility(View.GONE);    // After submitting the answer the textbox dissapears
+
+
+
             }
         });
+        numR = (EditText) findViewById(R.id.numRadio);
+
+        acceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                int num = Integer.parseInt(numR.getText().toString());
+                String str = listOfAnswers.get(num) + "    \n *Accepted*";
+                listOfAnswers.set(num, str);
+
+//                SpannableString str1 = new SpannableString(listOfAnswers.get(0));
+//                str1.setSpan(new ForegroundColorSpan(Color.RED), 0, str1.length(), 0);
+            }
+        });
+        declineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                int num = Integer.parseInt(numR.getText().toString());
+                String str = listOfAnswers.get(num) + "    \n *Declined*";
+                listOfAnswers.set(num, str);
+            }
+        });
+
+
+
     }
+//    public void onRadioButtonClicked(View view){
+//        boolean checked = ((RadioButton) view).isChecked();
+//        //SpannableStringBuilder builder = new SpannableStringBuilder();
+//        SpannableString str1 = new SpannableString(listOfAnswers.get(0));
+//        // Check which radio button was clicked
+//        switch(view.getId()) {
+//            case R.id.acceptButton:
+//                if (checked)
+//                    str1.setSpan(new ForegroundColorSpan(Color.RED), 0, str1.length(), 0);
+//                    break;
+//            case R.id.declineButton:
+//                if (checked)
+//                    // Ninjas rule
+//                    break;
+//        }
+//    }
+
 }
