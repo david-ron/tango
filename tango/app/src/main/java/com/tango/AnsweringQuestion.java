e593189a4cb3e4ac5ee6e02e70f3d6e80adbd638
Charles <chanc09@gmail.com>
2018-02-13 19:38:33 -0500

    #40 cleaned up code, added exception for arraylist out of bounds

--- tango/app/src/main/java/com/tango/AnsweringQuestion.java
+++ tango/app/src/main/java/com/tango/AnsweringQuestion.java
@@ -1,21 +1,13 @@
 package com.tango;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.SpannableStringBuilder;
 import android.widget.Button;
 import android.view.View;
-import android.content.Intent;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.text.style.ForegroundColorSpan;
 
 import java.util.ArrayList;
 
@@ -77,20 +69,20 @@ public class AnsweringQuestion extends AppCompatActivity {
 
                 textbox.setVisibility(View.GONE);    // After submitting the answer the textbox dissapears
 
-
-
             }
         });
         numR = (EditText) findViewById(R.id.numRadio);
+
+        //actions for accepting and declincing answers
         if(!listOfAnswers.isEmpty()) { // handles empty list
             acceptButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
-
                     int num = Integer.parseInt(numR.getText().toString()) - 1;
-                    String str = listOfAnswers.get(num) + "    \n *Accepted*";
-                    listOfAnswers.set(num, str);
+                    if(listOfAnswers.size() < num) {
+                        String str = listOfAnswers.get(num) + "    \n *Accepted*";
+                        listOfAnswers.set(num, str);
+                    }
 
                 }
             });
@@ -100,33 +92,12 @@ public class AnsweringQuestion extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
 
-
                     int num = Integer.parseInt(numR.getText().toString()) - 1;
                     String str = listOfAnswers.get(num) + "    \n *Declined*";
                     listOfAnswers.set(num, str);
                 }
-
             });
         }
-
-
-
     }
-//    public void onRadioButtonClicked(View view){
-//        boolean checked = ((RadioButton) view).isChecked();
-//        //SpannableStringBuilder builder = new SpannableStringBuilder();
-//        SpannableString str1 = new SpannableString(listOfAnswers.get(0));
-//        // Check which radio button was clicked
-//        switch(view.getId()) {
-//            case R.id.acceptButton:
-//                if (checked)
-//                    str1.setSpan(new ForegroundColorSpan(Color.RED), 0, str1.length(), 0);
-//                    break;
-//            case R.id.declineButton:
-//                if (checked)
-//                    // Ninjas rule
-//                    break;
-//        }
-//    }
 
 }
