52b436a212dd63a66b1898c5e810f3266a7662c8
DanielPri <privorotskyd@gmail.com>
2018-02-14 11:52:58 -0500

    #41 removed unnecessary code now that buttons are a class

--- tango/app/src/main/java/com/tango/VoteOnAnswersTest.java
+++ tango/app/src/main/java/com/tango/VoteOnAnswersTest.java
@@ -12,110 +12,20 @@ import org.w3c.dom.Text;
 
 public class VoteOnAnswersTest extends AppCompatActivity {
 
-    private int pointValue;
-
-
-    //the Vote enum is used to check the status of the vote
-    enum Vote
-    {
-        UNSELECTED, UPVOTED, DOWNVOTED;
-    }
-    private Vote vote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote_on_answers_test);
 
-        //TODO get value from database, don't just assign 0
-        pointValue = 0;
-        //TODO initial state of the enum info should be from database
-        vote = Vote.UNSELECTED;
-
-        //the pointage to be displayed is the Textview
-        final TextView pointQty = (TextView) findViewById(R.id.pointQty);
-        final ImageButton upvoteButton= (ImageButton) findViewById(R.id.upvote_button);
-        final ImageButton downvoteButton= (ImageButton) findViewById(R.id.downvote_button);
-        pointQty.setText(Integer.toString(pointValue));
-
         //Create button through java
         //First we need to set up the layout
         ConstraintLayout testLayout = (ConstraintLayout) findViewById(R.id.testLayout);
 
         //Then add button through the custom button object
-        VoteButtons testButton1 = new VoteButtons(this, testLayout, 200,200);
-        VoteButtons testButton2 = new VoteButtons(this, testLayout, 200,350);
+        VoteButtons testButton1 = new VoteButtons(this, testLayout, 200, 200);
+        VoteButtons testButton2 = new VoteButtons(this, testLayout, 200, 350);
 
-        //occurs when up arrow is pressed
-        upvoteButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //If no votes yet, upvoting will add a point.
-                //(up arrow becomes blue)
-                if(vote == Vote.UNSELECTED) {
-                    upvoteButton.setColorFilter(getResources().getColor(R.color.blue));
-                    pointQty.setText(Integer.toString(++pointValue));
-                    vote = Vote.UPVOTED;
-                    //TODO assign pointage value to Database
-                    //TODO assign vote status to Database
-                }
-                //if downvoted, upvoting adds 2 points
-                //(down arrow becomes black and up becomes blue)
-                else if(vote == Vote.DOWNVOTED){
-                    upvoteButton.setColorFilter(getResources().getColor(R.color.blue));
-                    downvoteButton.setColorFilter(getResources().getColor(R.color.black));
-                    pointValue++;
-                    pointQty.setText(Integer.toString(++pointValue));
-                    vote = Vote.UPVOTED;
-                    //TODO assign pointage value to Database
-                    //TODO assign vote status to Database
-                }
-                //if already upvoted, upvoting again reverts to unselected, so removes a point
-                //(up becomes black)
-                else if(vote == Vote.UPVOTED){
-                    upvoteButton.setColorFilter(getResources().getColor(R.color.black));
-                    pointQty.setText(Integer.toString(--pointValue));
-                    vote = Vote.UNSELECTED;
-                    //TODO assign pointage value to Database
-                    //TODO assign vote status to Database
-
-                }
-            }
-        });
-        //occurs when down arrow is pressed
-        downvoteButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            //If no votes yet, downvoting will remove a point.
-                //down becomes red
-                if(vote == Vote.UNSELECTED) {
-                    downvoteButton.setColorFilter(getResources().getColor(R.color.red));
-                    pointQty.setText(Integer.toString(--pointValue));
-                    vote = Vote.DOWNVOTED;
-                    //TODO assign pointage value to Database
-                    //TODO assign vote status to Database
-            }
-            //if upvoted, downvoting removes 2 points
-                //up becomes black, down becomes red
-                else if(vote == Vote.UPVOTED){
-                    upvoteButton.setColorFilter(getResources().getColor(R.color.black));
-                    downvoteButton.setColorFilter(getResources().getColor(R.color.red));
-                    pointValue--;
-                    pointQty.setText(Integer.toString(--pointValue));
-                    vote = Vote.DOWNVOTED;
-                    //TODO assign pointage value to Database
-                    // TODO assign vote status to Database
-                }
-            //if already downvoted, downvoting again reverts to unselected, so adds a point
-                //downbecomes black
-                else if(vote == Vote.DOWNVOTED) {
-                    downvoteButton.setColorFilter(getResources().getColor(R.color.black));
-                    pointQty.setText(Integer.toString(++pointValue));
-                    vote = Vote.UNSELECTED;
-                    //TODO assign pointage value to Database
-                    //TODO assign vote status to Database
-                }
-            }
-        });
     }
-
 
 }
