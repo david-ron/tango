de0b3bab238c6c88680905a8e2c08652da321eed
david-ron <ronci.david@gmail.com>
2018-02-28 11:22:31 -0500

    #60 adding basic list functionality from database

--- tango/app/src/main/java/com/tango/MasterQuestion.java
+++ tango/app/src/main/java/com/tango/MasterQuestion.java
@@ -25,8 +25,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -42,7 +45,7 @@ import java.util.ArrayList;
 public class MasterQuestion extends BaseActivity implements View.OnClickListener{
 
     private Button  newQuestionButton;
-    private ArrayList<TextView> Questions;
+    private ArrayList<Question> Questions;
     private EditText futureQuestion;
     private FirebaseAuth mAuth;
     private GoogleSignInClient mGoogleSignInClient;
@@ -78,8 +81,25 @@ public class MasterQuestion extends BaseActivity implements View.OnClickListener
         TextView textView = new TextView(this);
         textView.setText("New text");
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference =    databaseQuestion;
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot childDataSnapshot : dataSnapshot.getChildren()) {
+                   Question question =childDataSnapshot.getValue(Question.class);
+                   Questions.add(question);//gives the value for given keyname
+                    Log.i("viewing",Questions);
+                }
 
 
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
