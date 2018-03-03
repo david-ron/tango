c4a3a42e871e7355f2968f329231a013b9a13f7a
DanielPri <privorotskyd@gmail.com>
2018-03-03 18:44:09 -0500

    #61 Fragment_Answer.java initializes fragment_answer layout

--- tango/app/src/main/java/com/tango/Fragment_Answer.java
+++ tango/app/src/main/java/com/tango/Fragment_Answer.java
@@ -1,8 +1,23 @@
 package com.tango;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Play on 03/03/2018.
  */
 
-public class Fragment_Answer {
+public class Fragment_Answer extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_answer, container, false);
+        return view;
+    }
+
+    
 }
