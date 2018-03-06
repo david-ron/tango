c67055724714ab82e1482705599f338e22928ccc
SMHK <karimian_hassan@yanoo.ca>
2018-03-06 14:36:58 -0500

    #61 started writing test for ImprovedAnswer

--- app/src/androidTest/java/com/tango/ImprovedAnswerTest.java
+++ app/src/androidTest/java/com/tango/ImprovedAnswerTest.java
@@ -1,8 +1,36 @@
+package com.tango;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by Hking on 3/6/2018.
  */
 public class ImprovedAnswerTest {
+
+   @rule
+   public activityTestRule<ImprovedAnswer> mainActivityactivityTestRule = new ActivityTestRule<>();
+
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+
+    @test
+    public void testlunch(){
+
+    }
+
+
+
+    @After
+    public void tearDown() throws Exception {
+    }
 
 }
