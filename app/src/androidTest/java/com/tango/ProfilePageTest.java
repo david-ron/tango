582299ab3cc92e84313025ffac4039ea94262b0a
Charles <chanc09@gmail.com>
2018-03-18 19:24:43 -0400

    #79 2 tests for profile passed

---
+++ app/src/androidTest/java/com/tango/ProfilePageTest.java
@@ -0,0 +1,43 @@
+package com.tango;
+
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by charleschan on 2018-03-18.
+ */
+public class ProfilePageTest {
+    @Rule
+    public ActivityTestRule<ProfilePage> mActivityTestRule = new ActivityTestRule<>(ProfilePage.class);
+    private ProfilePage mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testLaunch1(){
+        View username = mActivity.findViewById(R.id.username);
+        assertNotNull(username);
+    }
+
+    @Test
+    public void testLaunch2(){
+        View pictureButton = mActivity.findViewById(R.id.changepicture);
+        assertNotNull(pictureButton);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+
+}
