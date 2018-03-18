4c48ea1fae0415d880689d72563d7930e7d5d557
Charles <chanc09@gmail.com>
2018-03-18 18:39:31 -0400

    #79 3 tests for question page, passed

---
+++ app/src/androidTest/java/com/tango/QuestionPageActivityTest.java
@@ -0,0 +1,47 @@
+package com.tango;
+
+import org.junit.After;
+import org.junit.Before;
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+import org.junit.Rule;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by charleschan on 2018-03-18.
+ */
+public class QuestionPageActivityTest {
+    @Rule
+    public ActivityTestRule<QuestionPageActivity> mActivityTestRule = new ActivityTestRule<>(QuestionPageActivity.class);
+    private QuestionPageActivity mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testLaunch1(){
+        View questionButton = mActivity.findViewById(R.id.fab_submit_post);
+        assertNotNull(questionButton);
+    }
+
+    @Test
+    public void testLaunch2(){
+        View questionView = mActivity.findViewById(R.id.field_title);
+        assertNotNull(questionView);
+    }
+
+    @Test
+    public void testLaunch3(){
+        View questionView = mActivity.findViewById(R.id.field_body);
+        assertNotNull(questionView);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+
+}
