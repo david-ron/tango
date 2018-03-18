08d8778168e22b16bbbe44ac3adde9ab93432f6f
Charles <chanc09@gmail.com>
2018-03-18 18:14:31 -0400

    #79 instramental testing, sign in page

---
+++ app/src/androidTest/java/com/tango/GoogleSignInActivityTest.java
@@ -0,0 +1,44 @@
+package com.tango;
+
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by charleschan on 2018-03-18.
+ */
+public class GoogleSignInActivityTest {
+
+    @Rule
+    public ActivityTestRule<GoogleSignInActivity> mActivityTestRule = new ActivityTestRule<>(GoogleSignInActivity.class);
+    private GoogleSignInActivity mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testLaunchTitle(){
+        View view = mActivity.findViewById(R.id.title_text);
+        assertNotNull(view);
+
+    }
+
+    @Test
+    public void testLaunchSignInButton(){
+        View view = mActivity.findViewById(R.id.sign_in_button);
+        assertNotNull(view);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+
+}
