517112f27c8772b37bde12fbad3e5e4d4d906372
Charles <chanc09@gmail.com>
2018-03-18 20:00:00 -0400

    #79 feed activity 1 test failed

---
+++ app/src/androidTest/java/com/tango/FeedActivityTest.java
@@ -0,0 +1,40 @@
+package com.tango;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+import android.widget.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by charleschan on 2018-03-18.
+ */
+public class FeedActivityTest {
+
+
+    @Rule
+    public ActivityTestRule<FeedActivity> mActivityTestRule = new ActivityTestRule<>(FeedActivity.class);
+    private FeedActivity mActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void testLaunch1(){
+        View tab = mActivity.findViewById(R.id.container);
+        assertNotNull(tab);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+
+}
